d711cf6cfa7e32c42c3034f94d7b3add45b7770b
YVPekarskyi <33490869+YVPekarskyi@users.noreply.github.com>
2017-11-19 00:17:40 +0200

    Update Home_Work_1_to_6.py

--- Home_Work_1_to_6.py
+++ Home_Work_1_to_6.py
@@ -1,7 +1,7 @@
 a = 15
 b = 4
 c = 120
-formula = a+b*(c/2)
+formula = a + b*(c / 2)
 result = "результат уравнения при a = %d, b = %d, с = %d равно %d" % (a, b, c, formula)
 print (result)
 
@@ -16,7 +16,7 @@ print (result)
 a = 355
 b = 76
 c = 98
-formula = (a+b)/12*c%4+b
+formula = (a + b) / 12*c % 4 + b
 result = "результат уравнения при a = %d, b = %d, с = %d равно %d" % (a, b, c, formula)
 print (result)
 
@@ -24,7 +24,7 @@ print (result)
 a = 8
 b = 12
 c = 2
-formula = (a - b * c ) / ( a + b ) % c
+formula = (a - b*c) / (a + b) % c
 result = "результат уравнения при a = %d, b = %d, с = %d равно %.d" % (a, b, c, formula)
 print (result)
 
@@ -33,7 +33,7 @@ import math
 a = 4
 b = -12
 c = 1
-formula = math.fabs(a - b)/math.pow((a + b), 3) - math.cos(c * math.pi / 180)
+formula = math.fabs(a - b) / math.pow((a + b), 3) - math.cos(c*math.pi / 180)
 result = "результат уравнения при a = %d, b = %d, с = %d равно %.4f" % (a, b, c, formula)
 print (result)
 
@@ -41,6 +41,6 @@ print (result)
 a = 100
 b = 5
 c = 25
-formula = math.pow ((math.log(1+c) / -b), 4) + math.fabs(a)
+formula = math.pow((math.log(1 + c) / -b), 4) + math.fabs(a)
 result = "результат уравнения при a = %d, b = %d, с = %d равно %.d" % (a, b, c, formula)
 print (result)
